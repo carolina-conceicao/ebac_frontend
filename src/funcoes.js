d481919cc2ea7b74e02fcabe0e49f678f97eb1d8
Carolina Barbosa da Conceição <carolina.barbosa.conceicao@gmail.com>
2023-06-05 03:26:03 -0300

    Initial commit

---
+++ src/funcoes.js
@@ -0,0 +1,13 @@
+"use strict";
+// Multiplicacao de dois numeros
+function calculaArea(base, altura) {
+    return base * altura;
+}
+const resultado = calculaArea(5, 10);
+console.log(resultado);
+//Saudacao
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+const saudadcaoMensagem = saudacao("Carolina");
+console.log(saudadcaoMensagem);
---
+++ src/funcoes.ts
@@ -0,0 +1,15 @@
+// Multiplicacao de dois numeros
+function calculaArea(base: number, altura: number): number {
+    return base * altura;
+}
+
+const resultado = calculaArea(5, 10)
+console.log(resultado)
+
+//Saudacao
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+const saudadcaoMensagem = saudacao("Carolina");
+console.log(saudadcaoMensagem);
